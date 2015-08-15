8f6312072bb97c95fb1b62d3e3fc59577a9db514
Puneeth Rai <npuneethrai@yahoo.co.in>
2015-08-15 22:37:48 +0530

    Timer functionality almost ready

--- twentyminute/Timer.cs
+++ twentyminute/Timer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Threading;
 
 namespace takeabreak
 {
@@ -9,6 +10,11 @@ namespace takeabreak
     {
         public int breakTimeLeft;
         public int breakDurationLeft;
+        public TimerInfo(int ibreakTimeLeft, int ibreakDurationLeft)
+        {
+            breakTimeLeft = ibreakTimeLeft;
+            breakDurationLeft = ibreakDurationLeft;
+        }
 
     }
     public struct TimerConfig
@@ -36,10 +42,10 @@ namespace takeabreak
     {
         #region private
         private TimerConfig tc;
-        private void resetCount()
-        {
-            
-        }
+        private Boolean paused;
+        private DispatcherTimer timer;
+        private Int16 ONESECOND = 1;
+        private TimerInfo ti;
         #endregion
         #region public
         public Timer():this(new TimerConfig(20*60, 20, null))
@@ -49,26 +55,64 @@ namespace takeabreak
         public Timer(TimerConfig iConfig)
         {
             tc = iConfig;
+            reset();
             if(tc.autoStart)
             {
                 start();
+            } else
+            {
+                pause();
             }
         }
         public void pause()
         {
-
+            paused = true;
         }
         public void resume()
         {
-
+            paused = false;
+        }
+        public Boolean isPaused()
+        {
+            return paused;
         }
         public void start()
         {
-
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0,ONESECOND);
+            timer.Start();
+            timer.Tick += Timer_Tick;
+            resume();
         }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (!this.isPaused())
+            {
+                if(ti.breakTimeLeft <= 0)
+                {
+                    ti.breakTimeLeft--;
+                } else if (ti.breakDurationLeft <= 0)
+                {
+                    ti.breakDurationLeft--;
+                } else
+                {
+                    reset();
+                }
+                if(tc.cb != null)
+                {
+                    tc.cb(ti);
+                }
+            }
+        }
+
         public void stop()
         {
-
+            timer.Stop();
+        }
+        public void reset()
+        {
+            ti = new TimerInfo(tc.breakTime, tc.breakDuration);
         }
         public void addCallback(Func<TimerInfo, TimerInfo> icb)
         {
